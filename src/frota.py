c4719c02cf125d53362b88a718670b52ac439440
ceitelabinfo <nicolima1905@gmail.om>
2024-08-28 08:56:29 -0300

    Mundanças no código

--- src/frota.py
+++ src/frota.py
@@ -2,9 +2,9 @@ class Carro:
     modelo : str
     marca : str
     cor : str
-    odometro = 0.0
-    motor_on = False
-    tanque = 0.0
+    __odometro = 0.0
+    __motor_on = False
+    __tanque = 0.0
     consumo_medio = float
 
     def __init__(self, modelo : str, marca : str, cor : str,
@@ -12,42 +12,45 @@ class Carro:
         self.modelo = modelo
         self.marca = marca
         self.cor = cor
-        self.odometro = odometro
-        self.motor_on = motor
-        self.tanque = tanque
+        self.__odometro = odometro
+        self.__moto_on = motor
+        self.__tanque = tanque
         self.consumo_medio = consumo_medio
 
     def ligar(self):
-        if not self.motor_on and self.tanque > 0:
-             self.motor_on = True
+        if not self.__moto_on and self.__tanque > 0:
+             self.__moto_on = True
         else:
             raise Exception("Erro: Motor já ligado ou o tanque está vazio!")
 
     def acelerar(self, velocidade : float, tempo : float):
-        if self.motor_on and self.tanque > 0:
+        if self.__moto_on and self.__tanque > 0:
             km = velocidade * tempo
             litros = km/self.consumo_medio
 
-            if self.tanque >= litros:
-                self.tanque -= litros
+            if self.__tanque >= litros:
+                self.__tanque -= litros
             else:
                 km = litros * self.consumo_medio
-                self.tanque = 0
+                self.__tanque = 0
                 self.desligar()
-            self.odometro += km
+            self.__odometro += km
 
         else:
             raise Exception('Erro: Não é possível acelerar! Motor desligado ou sem combustível!');
     def desligar(self):
-        if self.motor_on:
-            self.motor_on = False
+        if self.__moto_on:
+            self.__moto_on = False
         else:
             raise Exception("Erro: Motor já desligado!")
 
+    def get_odometro(self):
+        return self.__odometro
+
     def __str__(self): #todo objeto em python tem o metodo __str__
         info = (f'Carro {self.modelo}, marca {self.marca}, '
-                f'cor {self.cor}\n{self.odometro} Km, '
-                f'motor {self.motor_on} consumo {self.consumo_medio} km/l {self.tanque}')
+                f'cor {self.cor}\n{self.__odometro} Km, '
+                f'motor {self.__moto_on} consumo {self.consumo_medio} km/l {self.__tanque}')
         return info
 
 
--- src/main.py
+++ src/main.py
@@ -42,7 +42,7 @@ if __name__ == "__main__":
     '''
     Controlando 2 carros até ele atingir 600 Km
     '''
-    while carro1.odometro < 600 and carro2.odometro < 600 and (carro1.tanque > 0 or carro2.tanque > 0) :
+    while carro1.get_odometro() < 600 and carro2.get_odometro() < 600 and (carro1.get_tanque() > 0 or carro2.get_tanque() > 0) :
         try:
             op = 0
             while op not in (1,2):
